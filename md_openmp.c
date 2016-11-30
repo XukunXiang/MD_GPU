85cac973c24bfbc3e11a61be34a137933edbcc9b
XukunXiang <xxk08300190024@gmail.com>
2016-11-30 10:40:24 -0500

    first commit

---
+++ md_openmp.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+int main() {
+
+/*
+	Goals:
+	1. make this md c code working
+	2. implement OpenMP with this code and check with the fortran version
+	3. implement CUDA to compare the result with OpenMP
+	4. try to get PPPM working with GPU
+
+*/
+
+
+
+}
