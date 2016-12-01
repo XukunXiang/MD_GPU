f0346f15f70967567d0c4a3aecadd0be5abbcacd
XukunXiang <xxk08300190024@gmail.com>
2016-11-30 23:23:08 -0500

    pureMD without COE check

--- md_openmp.c
+++ md_openmp.c
@@ -18,8 +18,8 @@
 #include <omp.h>
 
 //simulation setup
-#define N 1000				//number of particles
-#define Ntime 1		//number of iternations
+#define N 100				//number of particles
+#define Ntime 10		//number of iternations
 #define newR 46022.8	//initial radius of bunch
 #define cutoff 20			//lower limit of the initial distance between electrons
 
@@ -28,9 +28,45 @@
 #define vc 5.85E3			//speed of light
 #define PI 3.141592653589793	//  \pi
 
-double inline getrand(){	return (double)rand()/(double)RAND_MAX;}
+double inline getrand(){ return (double)rand()/(double)RAND_MAX; }
 
-void verlet(double r[N][3]){
+void getForce(double f[N][3],double r[N][3]) {
+	int i,j,k;
+	double rel[3], rel_c, fij[3];
+	//use openmp here with (rel[3],rel_c) private
+	for (j=0; j<(N-1); j++) {
+		for (i=j+1; i<N; i++) {
+			rel_c = 0.0;
+			for (k=0; k<3; k++) {
+				rel[k] = r[i][k] - r[j][k];
+				rel_c += rel[k]*rel[k];
+			}
+			rel_c = - pow(rel_c,1.5 );
+			for (k=0; k<3; k++) {
+				fij[k] = rel[k]*rel_c;
+				//atomic operation here
+				f[j][k] -= fij[k];
+				f[i][k] += fij[k];
+			}
+		}
+	}
+}
+
+void verlet(double r[N][3],double v[N][3],double f[N][3], double dt){
+	int i,j;
+	double hdtm = 0.5*dt/m; 
+	for (i=0; i<N; i++) {
+		for (j=0; j<3; j++) {
+			v[i][j] += f[i][j]*hdtm;
+			r[i][j] += v[i][j]*dt;
+		}
+	}
+	getForce(f,r);
+	for (i=0; i<N; i++) {
+		for (j=0; j<3; j++) {
+			v[i][j] += f[i][j]*hdtm;
+		}
+	}
 }
 
 int main() {
@@ -79,7 +115,7 @@ int main() {
 //		fprintf(initR,"%5d %11.3f \t %11.3f \t %11.3f \n",1,R[i][0],R[i][1],R[i][2]);
 //	}
 	for (iter = 0; iter< Ntime; iter++) {
-		verlet(R);
+		verlet(R,V,F,dt);
 		realt += dt;
 /*
 		//output
@@ -88,8 +124,9 @@ int main() {
 */
 	}
 
-
 	fclose(RVo);
 	fclose(To);
 	fclose(initR);
+
+	return 0;
 }
